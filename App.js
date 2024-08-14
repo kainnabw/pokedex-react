242b4c92bb7c49d7046d68aabef5da1a3581e162
TymotheoTrisch <tymotheo.dev@gmail.com>
2024-08-14 01:49:06 -0300

    Criando scroll e get geral

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Button } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import axios from 'axios';
 import SearchInput from './components/SearchInput';
 import SearchResults from './components/SearchResult';
@@ -10,11 +8,21 @@ const App = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [searchType, setSearchType] = useState('general');
 
   const fetchPokemons = async () => {
-    if (query.trim() === '') return;
+    if (query.trim() === ''){
+      setPage(1);
+      setResults([])
+      setSearchType('general')
+      return;
+    } 
 
     setError(null);
+    setSearchType('query');
 
     try {
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${query}`);
@@ -25,21 +33,65 @@ const App = () => {
     }
   };
 
+  const fetchGeralPokemons = async () => {
+    if (loading) return;
+
+    setLoading(true);
+    setError(null);
+    setSearchType('general');
+    
+    try {
+      const offset = (page - 1) * 10;
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
+      const newResults = response.data.results || [];
+      
+      setResults(prevResults => [...prevResults, ...newResults]);
+      setHasMore(newResults.length > 0);
+    } catch (err) {
+      setError(err);
+      setResults([]);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (searchType === 'general') {
+      fetchGeralPokemons();
+      
+    }
+  }, [page]);
+
+  useEffect(() => {
+    if (searchType === 'query') {
+      fetchPokemons();
+    }
+  }, [query]);
+
+  const loadMorePokemons = () => {
+    if (hasMore && !loading) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.centralizador}>
-      <SearchInput
+        <SearchInput
+          value={query}
+          onChangeText={setQuery}
+          // onSubmit={fetchPokemons}
+        />
+        <Button
+          style={styles.buttonConsult}
+          title="Buscar"
+          onPress={fetchPokemons} />
 
-        value={query}
-        onChangeText={setQuery}
-        onSubmit={fetchPokemons}
-      />
-      <Button 
-      style={styles.buttonConsult} 
-      title="Buscar" 
-      onPress={fetchPokemons}/>
-      
-      <SearchResults results={results} error={error} />
+        <SearchResults
+          results={results}
+          error={error}
+          onLoadMore={loadMorePokemons}
+        />
       </View>
     </View>
   );
@@ -47,22 +99,22 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#D7D7D9',
     width: '100%',
-    position:"absolute"
   },
   centralizador: {
-    paddingTop:'15%',
+    paddingTop: '15%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#D7D7D9',
     width: '100%',
   },
   buttonConsult: {
-    width:'60%',
-    backgroundColor:'red'
+    width: '60%',
+    backgroundColor: 'red'
   }
 });
 
--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,39 +1,67 @@
-import React from 'react';
-import { FlatList, View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, View, StyleSheet, Text, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 import SearchResultItem from './SearchResultItem';
 
-const SearchResults = ({ results, error }) => {
-    if (error) {
-      return <Text style={styles.error}>Erro ao buscar dados.</Text>;
-    }
-  
-    if (results.length === 0) {
-      return <Text style={styles.noResults}>procure algum pokemon</Text>;
-    }
+const SearchResults = ({ results, error, onLoadMore }) => {
+  const [resultsPokemon, setResultsPokemon] = useState([]);
 
-    return (
-        <FlatList
-          data={results}
-          renderItem={({ item }) => <SearchResultItem item={item} />}
-          keyExtractor={(item) => item.name}
-         
-        />
-      );
+  useEffect(() => {
+    const resultInfoPokemons = async () => {
+      if (results.length === 0) return;
+
+      const infopokemons = [];
+      
+      try {
+        for (const pokemon of results) {
+          if (pokemon.name && pokemon.sprites) {
+            infopokemons.push(pokemon);
+          } else {
+            const response = await axios.get(pokemon.url);
+            infopokemons.push(response.data);
+          }
+        }
+        setResultsPokemon(infopokemons);
+      } catch (err) {
+        console.error(err);
+      }
     };
 
+    resultInfoPokemons();
+  }, [results]);
 
-    const styles = StyleSheet.create({
-        error: {
-          color: 'red',
-          textAlign: 'center',
-          marginTop: 20,
-        },
-        noResults: {
-          textAlign: 'center',
-          marginTop: 20,
-          fontSize: 18,
-          color: '#888',
-        },
-      });
+  if (error) {
+    return <Text style={styles.error}>Erro ao buscar dados.</Text>;
+  }
 
-      export default SearchResults;
+  if (resultsPokemon.length === 0) {
+    return <Text style={styles.noResults}>Procure algum Pokémon</Text>;
+  }
+
+  return (
+    <FlatList
+      data={resultsPokemon}
+      renderItem={({ item }) => <SearchResultItem item={item} />}
+      keyExtractor={(item) => item.name}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={<ActivityIndicator size="large" color="#0000ff" />}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  error: {
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  noResults: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 18,
+    color: '#888',
+  },
+});
+
+export default SearchResults;
